4513c6fd8769da7b60d834e68cc89c7169ef93a2
dvdgd <dvd.gd762@gmail.com>
2021-12-01 15:11:59 -0300

    Database connection

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const tasksRouter = require("./routes/tasksRoute");
+const connectMongo = require("./db/connectMongo");
+require("dotenv").config();
 
 const app = express();
 app.use(express.json());
 app.use("/api/v1/tasks", tasksRouter);
 
-app.listen(3000, console.log("server started on http://localhost:3000"));
+const start = async () => {
+	try {
+		await connectMongo(process.env.MONGO_URI);
+		app.listen(3000, console.log("server started on http://localhost:3000"));
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+start();
